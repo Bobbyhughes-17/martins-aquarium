d4c3a39ab967281a54a034de89b4ef39f9b6b7c7
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-02-23 15:05:39 -0500

    martins-aquarium

---
+++ scripts/database.js
@@ -0,0 +1,31 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            type of fish: "Blue",
+            name: "Bart",
+            food: "crustaceans",
+            size: 3,
+            location:"Texas"
+        },
+        {
+            type of fish: "Red",
+            name: "Bernie",
+            food: "philly cheesesteak",
+            size: 5,
+            location: "Outside in water"
+        },
+        {
+            type of fish: "Pink",
+            name: "Boo",
+            food: "hot dog",
+            size: 6,
+            location: "wvu"
+        }
+    ]
+}
